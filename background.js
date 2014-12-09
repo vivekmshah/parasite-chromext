1b8105375dec801c3f1460917f1d0771fe84955c
vivekmshah <vivek.shah@falkonry.com>
2014-12-08 20:03:33 -0800

    first commit - client-side querying of notes

---
+++ background.js
@@ -0,0 +1,18 @@
+console.log('Starting bg process');
+
+chrome.tabs.getSelected(null, function(tab) {
+	chrome.pageAction.show(tab.id);
+	console.log('The page action is now visible on tab #' + (tab.index+1));
+});
+
+chrome.tabs.onUpdated.addListener(function(tabID) {
+	chrome.pageAction.show(tabID);
+	console.log('The page action is visible on tab #' + tabID + 'that has just been updated');
+});
+
+chrome.pageAction.onClicked.addListener(function() {
+	chrome.tabs.getSelected(null, function(tab) {
+		chrome.tabs.sendRequest(tab.id, {callFunction: "toggleSidebar"}, function(){});
+		console.log('Sidebar is displayed for' + tab);	
+	});
+});
---
+++ script.js
@@ -0,0 +1,63 @@
+chrome.extension.onRequest.addListener(toggleSidebar);
+
+var sidebarOpen = false;
+
+function toggleSidebar() {
+
+	if(sidebarOpen) {
+		var el = document.getElementById('mySidebar');
+		el.parentNode.removeChild(el);
+		sidebarOpen = false;
+	}
+
+	else {
+		var sidebar = document.createElement('div');
+		sidebar.id = "mySidebar";
+
+		var domainName = document.domain;
+		var domain = domainName.replace('www.','');
+
+		console.log(document.domain);
+
+		httpRequest = new XMLHttpRequest();
+		httpRequest.onreadystatechange = function(){
+			if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+				var myArr = JSON.parse(httpRequest.responseText);
+				console.log(myArr);
+				sidebar.innerHTML = '';
+				for (var i = 0; i < myArr.length; i++){
+					if (myArr[i].domain==domain){
+						sidebar.innerHTML += myArr[i].description+'<br><br>';
+						console.log('yeah bitch!');
+					}
+				}
+			}
+		};
+		// httpRequest.open('GET', 'http://fast-ocean-4567.herokuapp.com/api/notes?domain=' + domain, true);
+
+		httpRequest.open('GET', 'http://fast-ocean-4567.herokuapp.com/api/notes', true);
+		httpRequest.send();
+
+		sidebar.style.cssText = "\
+			position:fixed;\
+			top:0px;\
+			left:0px;\
+			width:20%;\
+			height:110%;\
+			background:white;\
+			z-index:999999;\
+		";
+		
+		document.body.appendChild(sidebar);
+		sidebarOpen = true;
+	}
+}
+
+function myFunction(arr) {
+	var out = "";
+	var i;
+	for(i = 0; i < arr.length; i++) {
+		out += '<h4>' + arr[i].description + '</h4><br>';
+	}
+	return out;
+}
