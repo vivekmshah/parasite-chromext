bdb8d046a657fa2cc3034ecd741827ff1a6215e2
vivekmshah <vivek.shah@falkonry.com>
2014-12-10 22:51:38 -0800

    add wndx.js script to DOM in callback

--- script.js
+++ script.js
@@ -14,64 +14,26 @@ function toggleSidebar() {
 		var sidebar = document.createElement('div');
 		sidebar.id = "wndx-sidebar";
 
-		// var container = document.createElement('div');
-		// container.id = "wndx-container";
-
-		// sidebar.appendChild(container);
-
-		// var inputBox = document.createElement('input');
-		// inputBox.id = "wndx-input";
-
-		// sidebar.appendChild(inputBox);
-
-		// var domainName = document.domain;
-		// var domain = domainName.replace('www.','');
-
-		// httpRequest = new XMLHttpRequest();
-		// httpRequest.open('GET', '//fast-ocean-4567.herokuapp.com/api/notes?domain=' + domain, true);
-		// httpRequest.send();
-
-		// httpRequest.onreadystatechange = function(){
-		// 	if (httpRequest.readyState == 4 && httpRequest.status == 200) {
-		// 		var myArr = JSON.parse(httpRequest.responseText);
-		// 		// console.log(myArr);
-		// 		container.innerHTML = stylize(myArr);
-		// 	}
-		// };
-
 		var xhr = new XMLHttpRequest();
-		xhr.open('GET', '//fast-ocean-4567.herokuapp.com/wndx.html', true);
+		// xhr.open('GET', '//fast-ocean-4567.herokuapp.com/wndx.html', true);
+		xhr.open('GET', '//localhost:3000/wndx.html', true);
+
 		xhr.send();
 
 		xhr.onreadystatechange = function() {
 		    if (this.readyState !== 4) return;
 		    if (this.status !== 200) return;
 		    document.getElementById('wndx-sidebar').innerHTML = this.responseText;
-		};
 
-		sidebar.style.cssText = "\
-			position:absolute;\
-			top:0px;\
-			right:0px;\
-			width:400px;\
-			height:100%;\
-			background:#0e81ad;\
-			color: white;\
-			font-family: Sans-Serif;\
-			z-index:1000000;\
-			overflow: auto;\
-		";
+				var head = document.getElementsByTagName('head').item(0);
+		    var script = document.createElement('script');
+		    script.setAttribute('type', 'text/javascript');
+		    script.setAttribute('src', '//localhost:3000/wndx.js');
+		    head.appendChild(script);
+		};
 
 		document.body.appendChild(sidebar);
 		sidebarOpen = true;
-	}
-}
 
-function stylize(arr) {
-	var out = "";
-	var i;
-	for(i = 0; i < arr.length; i++) {
-		out += '<h4>' + arr[i].description + '</h4><br>';
 	}
-	return out;
 }
