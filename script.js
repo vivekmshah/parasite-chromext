52fcdaa0eb8afd45572f5d6ab2c436cfdf9ece8e
vivekmshah <vivek.shah@falkonry.com>
2014-12-16 11:21:17 -0800

    first attempt

--- script.js
+++ script.js
@@ -13,33 +13,59 @@ function toggleSidebar() {
 	else {
 		var sidebar = document.createElement('div');
 		sidebar.id = "wndx-sidebar";
-
+		
 		var xhr = new XMLHttpRequest();
-		xhr.open('GET', '//fast-ocean-4567.herokuapp.com/wndx.html', true);
+		xhr.open('GET', '//wndx-test.herokuapp.com/', true);
 
 		xhr.send();
 
 		xhr.onreadystatechange = function() {
 		    if (this.readyState !== 4) return;
 		    if (this.status !== 200) return;
-		    document.getElementById('wndx-sidebar').innerHTML = this.responseText;
+		    var wndxInfo = document.getElementById('wndx-sidebar');
+		    wndxInfo.innerHTML = this.responseText.replace(/<head>/, '<head><base target="_blank" href="//wndx-test.herokuapp.com/"/>');
+			};
+		
 
-				var head = document.getElementsByTagName('head').item(0);
-		    var script = document.createElement('script');
-		    script.setAttribute('type', 'text/javascript');
-		    script.setAttribute('src', '//fast-ocean-4567.herokuapp.com/wndx.js');
-		    head.appendChild(script);
-
-				var link = document.createElement('link');
-				link.rel = 'stylesheet';
-		    link.href = '//fonts.googleapis.com/css?family=Open+Sans';
-		    link.type = 'text/css';
-		    head.appendChild(link);
-
-		};
+		sidebar.style.cssText = "\
+			position:fixed;\
+			top:0px;\
+			right:0px;\
+			width:30%;\
+			height:110%;\
+			background:white;\
+			box-shadow:inset 0 0 1em black;\
+			z-index:999999;\
+			overflow:auto;\
+		";
 
 		document.body.appendChild(sidebar);
-		sidebarOpen = true;
+		$('#wndx-sidebar').mouseenter(function(){
+			$('#wndx-sidebar a').each(function () {
+   			 	var $this = $(this),
+        		href = $this.attr('href');
+    			$this.click(function (e, href) {
+            		e.preventDefault();
+        			var xhr = new XMLHttpRequest();
+        			xhr.open('GET', $this.attr('href'), true);
 
+        			xhr.send();
+        		xhr.onreadystatechange = function () {
+            		if (this.readyState !== 4) return;
+            		if (this.status !== 200) return;
+    			var wndxInfo = document.getElementById('wndx-sidebar');
+				wndxInfo.innerHTML = this.responseText.replace(/<head>/, '<head><base target="_blank" href="//wndx-test.herokuapp.com/"/>');
+			    	   
+
+
+			    	    };
+			    	});
+				}); 
+		});
+
+		
+		
+		sidebarOpen = true;
 	}
+	
 }
