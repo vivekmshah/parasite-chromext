38dd34e8ee18e6a9a66017148a01377e8ac838ce
vivekmshah <vivek.shah@falkonry.com>
2014-12-16 20:11:46 -0800

    extension integration with server - draft

--- script.js
+++ script.js
@@ -1,71 +1,62 @@
 chrome.extension.onRequest.addListener(toggleSidebar);
 
-var sidebarOpen = false;
+var mainOpen = false;
 
 function toggleSidebar() {
 
-	if(sidebarOpen) {
-		var el = document.getElementById('wndx-sidebar');
+	if(mainOpen) {
+		var el = document.getElementById('wndx-main');
 		el.parentNode.removeChild(el);
-		sidebarOpen = false;
+		mainOpen = false;
 	}
 
 	else {
 		var sidebar = document.createElement('div');
-		sidebar.id = "wndx-sidebar";
+		sidebar.id = "wndx-main";
+		// sidebar.innerHTML = sidebar.innerHTML.replace("/</div>/",'</div><base target="_blank" href="//fast-ocean-4567.herokuapp.com/"/>');
 		
 		var xhr = new XMLHttpRequest();
-		xhr.open('GET', '//wndx-test.herokuapp.com/', true);
+		xhr.open('GET', '//fast-ocean-4567.herokuapp.com/', true);
 
 		xhr.send();
 
 		xhr.onreadystatechange = function() {
 		    if (this.readyState !== 4) return;
 		    if (this.status !== 200) return;
-		    var wndxInfo = document.getElementById('wndx-sidebar');
-		    wndxInfo.innerHTML = this.responseText.replace(/<head>/, '<head><base target="_blank" href="//wndx-test.herokuapp.com/"/>');
+		    var wndxInfo = document.getElementById('wndx-main');
+		    wndxInfo.innerHTML = this.responseText;
+
+				var head = document.getElementsByTagName('head').item(0);
+		    var script = document.createElement('script');
+		    script.setAttribute('type', 'text/javascript');
+		    script.setAttribute('src', '//fast-ocean-4567.herokuapp.com/wndx.js');
+		    head.appendChild(script);
+
+		    var style = document.createElement('link');
+		    style.setAttribute('type', 'text/css');
+		    style.setAttribute('href', '//fast-ocean-4567.herokuapp.com/wndx.css');
+		    style.setAttribute('rel', 'stylesheet');
+		    head.appendChild(style);
+
 			};
 		
-
-		sidebar.style.cssText = "\
-			position:fixed;\
-			top:0px;\
-			right:0px;\
-			width:30%;\
-			height:110%;\
-			background:white;\
-			box-shadow:inset 0 0 1em black;\
-			z-index:999999;\
-			overflow:auto;\
-		";
+		// sidebar.style.cssText = "\
+		// 	position:fixed;\
+		// 	top:0px;\
+		// 	right:0px;\
+		// 	width:30%;\
+		// 	height:110%;\
+		// 	background:white;\
+		// 	box-shadow:inset 0 0 1em black;\
+		// 	z-index:999999;\
+		// 	overflow:auto;\
+		// ";
 
 		document.body.appendChild(sidebar);
-		$('#wndx-sidebar').mouseenter(function(){
-			$('#wndx-sidebar a').each(function () {
-   			 	var $this = $(this),
-        		href = $this.attr('href');
-    			$this.click(function (e, href) {
-            		e.preventDefault();
-        			var xhr = new XMLHttpRequest();
-        			xhr.open('GET', $this.attr('href'), true);
-
-        			xhr.send();
-        		xhr.onreadystatechange = function () {
-            		if (this.readyState !== 4) return;
-            		if (this.status !== 200) return;
-    			var wndxInfo = document.getElementById('wndx-sidebar');
-				wndxInfo.innerHTML = this.responseText.replace(/<head>/, '<head><base target="_blank" href="//wndx-test.herokuapp.com/"/>');
-			    	   
 
 
-			    	    };
-			    	});
-				}); 
-		});
+		};
 
-		
-		
-		sidebarOpen = true;
+		mainOpen = true;
 	}
 	
-}
